c7568a08eb3d95e0b7a207e539f9cc632279694b
Dean Eigenmann <dean.eigenmann@icloud.com>
2019-12-01 21:27:09 +0100

    Create 1.go

---
+++ 2019/1.go
@@ -0,0 +1,33 @@
+func main() {
+
+  var data = []int{}
+
+	f := 0
+	fuelOfFuel := 0
+
+	for _, d := range data  {
+		f += fuel(d)
+		fuelOfFuel += fuelOfFuel(d)
+	}
+}
+
+func fuelOfFuel(input int) int {
+	if input == 0 {
+		return input
+	}
+
+	n := input / 3
+	if n < 2 {
+		n = 0
+	} else {
+		n = n-2
+	}
+
+	input = n
+	
+	return input + fuelOfFuel(input)
+}
+
+func fuel(input int) int {
+	return (input / 3) - 2
+}
